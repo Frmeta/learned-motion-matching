61a5a825741b6bf9ebd181557db882e27df25fff
Fredo <fredotanzil@gmail.com>
2026-02-21 19:33:22 +0700

    organize file

--- controller.cpp
+++ controller.cpp
@@ -1238,7 +1238,7 @@ int main(void)
     
     // Ground Plane
     
-    Shader ground_plane_shader = LoadShader("./resources/checkerboard.vs", "./resources/checkerboard.fs");
+    Shader ground_plane_shader = LoadShader("./resources/shaders/checkerboard.vs", "./resources/shaders/checkerboard.fs");
     Mesh ground_plane_mesh = GenMeshPlane(20.0f, 20.0f, 10, 10);
     Model ground_plane_model = LoadModelFromMesh(ground_plane_mesh);
     ground_plane_model.materials[0].shader = ground_plane_shader;
@@ -1246,9 +1246,9 @@ int main(void)
     // Character
     
     character character_data;
-    character_load(character_data, "./resources/character.bin");
+    character_load(character_data, "./resources/bin/character.bin");
     
-    Shader character_shader = LoadShader("./resources/character.vs", "./resources/character.fs");
+    Shader character_shader = LoadShader("./resources/shaders/character.vs", "./resources/shaders/character.fs");
     Mesh character_mesh = make_character_mesh(character_data);
     Model character_model = LoadModelFromMesh(character_mesh);
     character_model.materials[0].shader = character_shader;
@@ -1256,7 +1256,7 @@ int main(void)
     // Load Animation Data and build Matching Database
     
     database db;
-    database_load(db, "./resources/database.bin");
+    database_load(db, "./resources/bin/database.bin");
     
     float feature_weight_foot_position = 0.75f;
     float feature_weight_foot_velocity = 1.0f;
@@ -1272,7 +1272,7 @@ int main(void)
         feature_weight_trajectory_positions,
         feature_weight_trajectory_directions);
         
-    database_save_matching_features(db, "./resources/features.bin");
+    database_save_matching_features(db, "./resources/bin/features.bin");
    
     // Pose & Inertializer Data
     
@@ -1474,9 +1474,9 @@ int main(void)
     bool lmm_enabled = false;
     
     nnet decompressor, stepper, projector;    
-    nnet_load(decompressor, "./resources/decompressor.bin");
-    nnet_load(stepper, "./resources/stepper.bin");
-    nnet_load(projector, "./resources/projector.bin");
+    nnet_load(decompressor, "./resources/bin/decompressor.bin");
+    nnet_load(stepper, "./resources/bin/stepper.bin");
+    nnet_load(projector, "./resources/bin/projector.bin");
 
     nnet_evaluation decompressor_evaluation, stepper_evaluation, projector_evaluation;
     decompressor_evaluation.resize(decompressor);
